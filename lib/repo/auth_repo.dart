fdbae31fd788f5f158c030c5887d8fb4d2ac83c4
TopiaAmr <a.alsaadny@najdahksa.com>
2022-10-28 15:25:16 +0200

    Adding the signup response

--- lib/repo/auth_repo.dart
+++ lib/repo/auth_repo.dart
@@ -1,21 +1,35 @@
-import 'dart:developer';
+import 'dart:convert';
 
+import 'package:docs_clone_flutter/constants.dart';
+import 'package:docs_clone_flutter/models/error_model.dart';
 import 'package:docs_clone_flutter/models/user_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart';
 
 final authRepoProvider = Provider(
   (ref) => AuthRepo(
     GoogleSignIn(),
+    Client(),
   ),
 );
 
+final userProvider = StateProvider<UserModel?>((ref) => null);
+
 class AuthRepo {
   final GoogleSignIn _googleSignIn;
+  final Client _client;
 
-  AuthRepo(GoogleSignIn googleSignIn) : _googleSignIn = googleSignIn;
+  AuthRepo(GoogleSignIn googleSignIn, Client client)
+      : _googleSignIn = googleSignIn,
+        _client = client;
 
-  void signInWithGoogle() async {
+  Future<ErrorModel> signInWithGoogle() async {
+    ErrorModel error = ErrorModel(
+      error: 'Some unexpected error occurred.',
+      data: null,
+    );
+
     final GoogleSignInAccount? user = await _googleSignIn.signIn();
     if (user != null) {
       final UserModel userAcc = UserModel(
@@ -25,6 +39,25 @@ class AuthRepo {
         token: '',
         uid: '',
       );
+      Response res = await _client.post(
+        Uri.parse('$kHost/api/signup'),
+        body: userAcc.toJson(),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+      switch (res.statusCode) {
+        case 200:
+          final newUser = userAcc.copyWith(
+            uid: jsonDecode(res.body)['user']['_id'],
+          );
+          error = ErrorModel(error: null, data: newUser);
+          break;
+        default:
+          error = ErrorModel(error: jsonDecode(res.body)['error'], data: null);
+          throw error.error ?? 'Something went wrong';
+      }
     }
+    return error;
   }
 }
