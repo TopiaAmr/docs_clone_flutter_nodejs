8ac5de0651eb57989a39fa2003fecbc0208e3b1b
TopiaAmr <a.alsaadny@najdahksa.com>
2022-10-28 13:43:07 +0200

    configure dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:docs_clone_flutter/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const ProviderScope(child: MyApp()));
 }
 
--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
+require('dotenv').config()
 const express = require("express")
 const mongoose = require("mongoose");
 const authRouter = require("./routes/auth");
 
 const PORT = process.env.PORT | 3001
-const PASSWORD = process.env.MONGO_PASSWORD
-const DB = `mongodb+srv://topia:${PASSWORD}@cluster0.bcnqe9q.mongodb.net`;
+const DB = process.env.mongoDB;
 
 const app = express()
 app.use(express.json())
