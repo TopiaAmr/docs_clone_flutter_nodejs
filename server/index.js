c193274419487f7718006a857b7d4b3a618f9156
TopiaAmr <a.alsaadny@najdahksa.com>
2022-10-28 15:24:56 +0200

    setup cors in server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 const express = require("express")
 const mongoose = require("mongoose");
+const cors = require('cors');
 const authRouter = require("./routes/auth");
 
 const PORT = process.env.PORT | 3001
 const DB = process.env.mongoDB;
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(authRouter)
 
